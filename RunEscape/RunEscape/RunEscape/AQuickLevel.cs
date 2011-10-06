5baa0d4f5c6fd132bec2b674b6b07340d9ecf4ac
Charlie Guse <charlesguse@users.noreply.github.com>
2011-10-06 16:06:33 -0400

    Forgot to check this in before. Didn't really matter. It was just changing the layout of the "tutorial" level.

--- RunEscape/RunEscape/RunEscape/AQuickLevel.cs
+++ RunEscape/RunEscape/RunEscape/AQuickLevel.cs
@@ -1,6 +1,8 @@
 ﻿//Press_escape_to_begin._Welcome_To_Run_Escape._Space_stops_you_and_escape_makes_run_you jump
 //Yo_dawg,_I_heard_you_like_escaping_from_the_Ludum_Dare_monster_with_your_escaped_character_while_pressing_escape
-namespace RunEscape{ //_making_this_a_bigger_platform_since_it_is_the_first_level_and_all
+namespace RunEscape
+{
+    //_making_this_a_bigger_platform_since_it_is_the_first_level_and_all
     //_This_doesn't_do_anything._I_just_wanted_to_make_it_the_first_level.
     class AQuickLevel{}}
 //Onto_the_code_now
